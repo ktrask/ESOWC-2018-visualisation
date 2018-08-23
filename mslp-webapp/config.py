cdacfca4c11466f3fd0994e3441b97e88596707e
Helena Schmidt <kt003@ktrask.de>
2018-08-23 14:39:08 +0200

    removed files not needed anymore

--- mslp-webapp/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'chee7NohBohmo7io7oow0ohQu'
-
-
-
-
--- mslp-webapp/run.py
+++
@@ -1,3 +0,0 @@
-#!venv/bin/python
-from app import app
-app.run(debug=True, host="0.0.0.0", port = 5003)
