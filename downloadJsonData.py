66182fc94e1a6df16f675662cf9369b64292ea32
Helena Schmidt <kt003@ktrask.de>
2018-08-24 12:26:39 +0200

    added commandline options to downloadJsonData

--- downloadJsonData.py
+++ downloadJsonData.py
@@ -1,16 +1,10 @@
-import requests
 from datetime import date, timedelta
-import time
-import random
-import json
-import sys 
+import time, json, sys
 from pathlib import Path
 from geopy.geocoders import Nominatim
 from tzwhere import tzwhere
-import asyncio
-import aiohttp
-import async_timeout
-
+import asyncio, async_timeout, aiohttp
+import getopt
 
 home = str(Path.home())
 with open(home + "/.ecmwfapirc") as fp:
@@ -25,6 +19,8 @@ api  = { "url": "https://api.ecmwf.int/v1/services/meteogram/requests/",
 #Replace XXXX with your ECMWF token : Token can found at https://api.ecmwf.int/v1/key/
 
 allMeteogramData = {}
+latitude = 0
+longitude = 0
 
 async def getData(session, param):
     with async_timeout.timeout(10):
@@ -32,7 +28,7 @@ async def getData(session, param):
         request =  {
                             "meteogram": "10days",
                             "param": param,
-                            "location": "%f/%f" % (loc.latitude, loc.longitude),
+                            "location": "%f/%f" % (latitude, longitude),
                             "date": yesterday.strftime('%Y%m%d'),# Date should be expresed as YYYYMMDD
                             "time": "0000"   # Time should bb expessed as HHHH [1200 or 0000]
                         }
@@ -55,20 +51,49 @@ async def main(loop):
         tasks = [getData(session, param) for param in params]
         await asyncio.gather(*tasks)
 
+def getCoordinates(argv):
+    if len(argv) > 0:
+        try:
+            opts, args = getopt.getopt(argv, "h", ["location=", "lat=","lon="])
+        except getopt.GetoptError:
+            print("downloadJsonData.py --location 'Braunschweig, Germany'")
+            print("downloadJsonData.py --lat 20 --lon 10")
+            sys.exit(2)
+        #print(opts)
+        for opt, arg in opts:
+            if opt == "-h":
+                print("downloadJsonData.py --location 'Braunschweig, Germany'")
+                print("downloadJsonData.py --lat 20 --lon 10")
+                sys.exit(0)
+            elif opt == "--location":
+                #print("location", arg)
+                geolocator = Nominatim()
+                loc = geolocator.geocode(arg)
+                latitude = loc.latitude
+                longitude = loc.longitude
+            elif opt == "--lat":
+                latitude = float(arg)
+            elif opt == "--lon":
+                longitude = float(arg)
+
+    else:
+        location = "Braunschweig Germany"
+        geolocator = Nominatim()
+        loc = geolocator.geocode(args)
+        latitude = loc.latitude
+        longitude = loc.longitude
+    return ( latitude, longitude )
 if __name__ == '__main__':
     startTime = time.time()
     tz = tzwhere.tzwhere()
-    if len(sys.argv) > 1:
-        location = sys.argv[1]
-    else:
-        location = "Braunschweig Germany"
-    geolocator = Nominatim()
-    loc = geolocator.geocode(location)
-    timezone = tz.tzNameAt(loc.latitude, loc.longitude)
+    latitude, longitude = getCoordinates(sys.argv[1:])
+    timezone = tz.tzNameAt(latitude, longitude)
     midTime = time.time()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(loop))
+    print(latitude)
+    print(longitude)
     #print(allMeteogramData)
     endTime = time.time()
-    #print("starting up: ", midTime-startTime)
-    #print("downloading: ", endTime-midTime)
+    print("starting up: ", midTime-startTime)
+    print("downloading: ", endTime-midTime)
