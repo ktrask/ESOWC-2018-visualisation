a72a52558be1b502198cc55fba83909e9bf17b60
Helena Schmidt <kt003@ktrask.de>
2018-08-24 18:36:32 +0200

    added dotted lines and hours

--- plotMeteogram.py
+++ plotMeteogram.py
@@ -10,6 +10,39 @@ from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 
 
+def getNextDottedHour(hour):
+    if hour < 2:
+        return 2
+    if hour < 10:
+        return 10
+    if hour < 14:
+        return 14
+    if hour < 22:
+        return 22
+    raise ValueError
+
+def getDottedHours(fromDate, toDate):
+    newDate = fromDate
+    dottedDates = []
+    while newDate < toDate:
+        try:
+            newDate = datetime.datetime(newDate.year, newDate.month, newDate.day, getNextDottedHour(newDate.hour))
+        except ValueError:
+            newDate = datetime.datetime(newDate.year, newDate.month, newDate.day, 2) + datetime.timedelta(1)
+        dottedDates.append(newDate)
+    return(dottedDates[:-1])
+
+
+def getNumberedHours(fromDate, toDate):
+    newDate = fromDate
+    numberedDates = []
+    while newDate < toDate:
+        if newDate.hour < 12:
+            newDate = datetime.datetime(newDate.year, newDate.month, newDate.day, 12)
+        else:
+            newDate = datetime.datetime(newDate.year, newDate.month, newDate.day, 0) + datetime.timedelta(1)
+        numberedDates.append(newDate)
+    return(numberedDates[:-1])
 
 
 def plotTemperature(ax, qdata, fromIdx, toIdx):
@@ -19,7 +52,18 @@ def plotTemperature(ax, qdata, fromIdx, toIdx):
     ax.fill_between(x= dates[fromIdx:toIdx], y1=np.array(qdata['2t']['twenty_five'][fromIdx:toIdx])-273.15 , y2=np.array(qdata['2t']['seventy_five'][fromIdx:toIdx])-273.15, color="blue", alpha = 0.5)
     #ax.plot(x = dates, y = np.array(qdata['2t']['median']) - 273.15, color="green")
     ax.plot_date(x = dates[fromIdx:toIdx], y = np.array(qdata['2t']['median'][fromIdx:toIdx]) - 273.15, color="black", linestyle="solid", marker=None)
+    dottedHours = getDottedHours(dates[fromIdx], dates[toIdx-1])
+    ymin, ymax = ax.get_ylim()
+    #print(dottedHours)
+    ax.vlines(dottedHours, ymin, ymax, linestyle = ':', color = "gray")
+    numberedHours = getNumberedHours(dates[fromIdx], dates[toIdx-1])
+    print(numberedHours)
     ax.yaxis.set_major_formatter(FormatStrFormatter('%d'+'\N{DEGREE SIGN}'+'C'))
+    for hour in numberedHours:
+        ax.text(hour, ymin, str(hour.hour), horizontalalignment = "center")
+    #ax.axis('off')
+    #ax.box(on=None)
+    ax.get_xaxis().set_visible(False)
 
 def plotWindBft(ax, qdata, fromIdx, toIdx):
     #vsupFilenames = ["rain_fuzzy.png", "rain_fuzzynotraining.png", "rain_fuzzyraining.png", "rain_norain.png", "rain_lightrain.png", "rain_rain.png", "rain_strongrain.png"]
@@ -140,10 +184,10 @@ if __name__ == '__main__':
             allMeteogramData['tp'] = json.load(fp)
         with open("data/ws-10days.json", "r") as fp:
             allMeteogramData['ws'] = json.load(fp)
-    fromIndex, toIndex = getTimeFrame(allMeteogramData, today, today + datetime.timedelta(3))
+    fromIndex, toIndex = getTimeFrame(allMeteogramData, today, today + datetime.timedelta(4))
     plt.figure(figsize=(14,6))
     #plt.xkcd()
-    gs = gridspec.GridSpec(3, 1, height_ratios=[2, 2, 4])
+    gs = gridspec.GridSpec(3, 1, height_ratios=[1, 1, 4])
     ax1 = plt.subplot(gs[1])
     ax2 = plt.subplot(gs[0])
     ax3 = plt.subplot(gs[2])
@@ -151,6 +195,6 @@ if __name__ == '__main__':
     plotPrecipitationVSUP(ax1, allMeteogramData['tp']['tp'], fromIndex, toIndex)
     plotTemperature(ax3, allMeteogramData['2t'], fromIndex, toIndex)
     plotWindBft(ax2, allMeteogramData['ws']['ws'], fromIndex, toIndex)
-    plt.gcf().autofmt_xdate()
+    #plt.gcf().autofmt_xdate()
     plt.savefig("./output/forecast.png", dpi = 300)
 
