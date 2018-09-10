e8e284376126da13d045f14cd69124aa5612415e
Helena Schmidt <kt003@ktrask.de>
2018-09-10 19:47:36 +0200

    changed request from post to get

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -22,37 +22,63 @@ class searchForm(FlaskForm):
 
 @app.route('/', methods=("GET", "POST"))
 def index():
-    form = searchForm(csrf_enable=True)
+    form = searchForm()
     if form.validate_on_submit():
         return redirect('/search')
     return render_template("index.html",
                            title='VSUP - Meteogram',
                            form = form)
 
-@app.route('/getPlot', methods=("GET", "POST"))
-def getInformation():
-    print('latitude: ' +  request.form['latitude'])
-    print('longitude: ' + request.form['longitude'])
-    print('location: ' +  request.form['location'])
-    filename = plotMeteogramFile(request.form['latitude'], request.form['longitude'], request.form['location'])
-    return jsonify( filename )
 
 @app.route('/search', methods=("GET", "POST"))
 def search():
     #print('latitude: ' +  request.form['latitude'])
     #print('longitude: ' + request.form['longitude'])
-    form = searchForm(csrf_enable=True)
+    #form = searchForm(csrf_enable=False)
+    #print(form)
+    print(request.args)
+    print([key for key in request.args.keys()])
+    #print('latitude: ' +  request.form['lat'])
+    #print('longitude: ' + request.form['lon'])
+    form = searchForm()
+    if request.method == 'GET':
+        print("lon", request.args['lon'])
+        if request.args['search']:
+            searchLocation = str(request.args['search'])
+            form.search.data = searchLocation
+            print(searchLocation)
+        else:
+            searchLocation = ""
+        if request.args['lat']:
+            latitude = float(request.args['lat'])
+            form.lat.data = latitude
+        else:
+            latitude = None
+        if request.args['lon']:
+            longitude = float(request.args['lon'])
+            form.lon.data = longitude
+        else:
+            longitude = None
+        days = int(request.args['days'])
+        form.days.data = days
+        plotType = str(request.args['plotType'])
+        form.plotType.data = plotType
     if form.validate_on_submit():
         #print(form.search.data)
         #print(form.days.data)
         searchLocation = form.search.data
         latitude = form.lat.data
         longitude = form.lon.data
+        days = form.days.data
+        plotType = form.plotType.data
         print('location: ' +  searchLocation)
+    else:
+        print("invalid form")
+    if "latitude" in locals():
         filename = plotMeteogramFile(latitude = latitude, longitude = longitude,
                                      location = searchLocation,
-                                     days = form.days.data,
-                                     plotType = form.plotType.data)
+                                     days = days,
+                                     plotType = plotType)
         with open("/tmp/"+filename, "rb") as fp:
             fileContent = b64encode(fp.read())
         #return jsonify( filename )
@@ -62,8 +88,6 @@ def search():
                 plotType = form.plotType.data,
                 image = 'data:image/png;base64,{}'.format(fileContent.decode())
                 )
-    else:
-        print("invalid form")
     return render_template("index.html",
                            title = 'VSUP - Meteogram',
                            form = form)
